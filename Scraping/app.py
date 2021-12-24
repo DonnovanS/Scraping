b7ff1f39a6d628ac50085f75824a20fd4e917684
DonnovanS <donovanespitia@gmail.com>
2021-12-23 23:30:09 -0500

    Primer commit

--- Scraping/app.py
+++ Scraping/app.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>My Flask1</h1>'
+@app.route("/urlscra", methods=["POST"])
+def urlscra():
+    json=request.get_json(force=True)
 
 if __name__ == '__main__':
     app.run(port=5000)
