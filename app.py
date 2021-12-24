cc2c26e38d7296e346a1c7ee3edffa7140864d35
DonnovanS <donovanespitia@gmail.com>
2021-12-24 12:00:14 -0500

    make it better

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask,request
+app = Flask(__name__)
+
+@app.route("/urlscra")
+def index():
+    return '<h1>Prueba</h1>'
+@app.route("/urlscra", methods=["POST"])
+def urlscra():
+    json=request.get_json(force=True)
+
+if __name__ == '__main__':
+    app.run()
